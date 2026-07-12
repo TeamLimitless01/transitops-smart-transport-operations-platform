903d003f0906560f85f7312a17c7af6737cc1681
Deepak Dhakad <145159816+DeepakDkd@users.noreply.github.com>
2026-07-12 12:43:21 +0530

    feat: add initial page components for dashboard, drivers, expenses, fuel logs, and maintenance

---
+++ app/(dashboard)/expences/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Expences() {
+  return (
+    <div>Expences</div>
+  )
+}
+
+export default Expences
---
+++ app/(dashboard)/fuel-logs/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function FuelLogs() {
+  return (
+    <div>FuelLogs</div>
+  )
+}
+
+export default FuelLogs
