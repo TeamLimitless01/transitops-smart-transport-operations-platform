41e7fae4269f72b3d18f6fad984109f4b4bb9955
Deepak Dhakad <145159816+DeepakDkd@users.noreply.github.com>
2026-07-12 12:43:36 +0530

    Refactor code structure for improved readability and maintainability

--- app/expences/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Expences() {
-  return (
-    <div>Expences</div>
-  )
-}
-
-export default Expences
--- app/fuel-logs/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function FuelLogs() {
-  return (
-    <div>FuelLogs</div>
-  )
-}
-
-export default FuelLogs
